611a33df4d79a451f1171f0a46cd54427e95dbdb
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2023-07-01 15:37:49 +0800

    Fix nav bar buttons overlap with main title.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 import { searchWeather } from "./weather.js";
 
-searchWeather(
-  document.querySelector(".primary"),
-  document.querySelector(".weatherInfo"),
-  document.querySelector(".newsPanel")
-);
+// searchWeather(
+//   document.querySelector(".primary"),
+//   document.querySelector(".weatherInfo"),
+//   document.querySelector(".newsPanel")
+// );
 
 (() => {
   const github = document.querySelector(".github");
@@ -12,17 +12,3 @@ searchWeather(
     window.open("https://github.com/Emman-pip");
   };
 })();
-
-() => {
-  const target = document.querySelectorAll(".carousel-item");
-  target.forEach((element) => {
-    document.querySelector("body").onscroll = () => {
-      const scroll = document.scrollingElement.scrollTop;
-
-      const xvalue = "center";
-      const scrollfactor = 0.3;
-      const yValue = scroll * scrollfactor;
-      element.style.backgroundPosition = xvalue + " " + yValue + "px";
-    };
-  });
-};
