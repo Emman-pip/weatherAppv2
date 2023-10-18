001499a2006e998ea5760a913caeae9d9168f57a
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2023-10-18 13:39:44 +0800

    add comment

--- loading.js
+++ loading.js
@@ -1,3 +1,5 @@
+// TODO: add a function to detect the loading of API
+
 export function loadingScreen() {
   const screen = document.querySelector(".loadingBlur");
   screen.classList.toggle("appear");
